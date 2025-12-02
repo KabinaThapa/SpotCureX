e8852f8c302a7eeb40a277922f31961fed539011
Kabina Thapa <kabinathapa@Mac.lan>
2025-12-01 20:49:35 -0600

    edit

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -17,14 +17,14 @@ export default function LandingPage() {
       <Navbar />
 
       {/* HERO SECTION */}
-      <section className="w-full bg-stone-600 relative flex items-center h-screen">
+      <section className="w-full bg-stone-600 relative flex items-center">
 
         {/* FULL HERO DARK OVERLAY */}
-        <div className="absolute inset-0 bg-stone-900/40 pointer-events-none z-10 h-full"></div>
+        <div className="absolute inset-0 bg-stone-900/40 pointer-events-none z-10"></div>
 
         {/* IMAGE */}
-        <div className="md:w-1/2 w-full flex items-center justify-center overflow-hidden h-full">
-          <div className="w-full h-full relative" style={{ aspectRatio: "1024/1536" }}>
+        <div className="md:w-1/2 w-full h-screen overflow-hidden border" >
+          <div className="w-full h-full relative border" style={{ aspectRatio: "1024/1536" }}>
             <img
               src={women}
               alt="women with pimple patches"
